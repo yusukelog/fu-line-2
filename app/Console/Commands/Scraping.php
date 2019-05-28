46d231af6767f96cd7d31ba712baa17935034ea0
koga <web@yusukelog.com>
2019-05-28 23:57:25 +0900

    script semi-complete

--- app/Console/Commands/Scraping.php
+++ app/Console/Commands/Scraping.php
@@ -39,32 +39,43 @@ class Scraping extends Command
      */
     public function handle()
     {
-        global $names,$time,$message;
+        global $names,$time,$message,$week;
         $names = ['中山','水城','竹内','小野','桐谷','亀井'];
+
         $goutte = GoutteFacade::request('GET', 'https://www.cityheaven.net/kanagawa/A1403/A140301/elegaku/attend/');
         $goutte->filter('#contents_main')->each(function ($ul) {
-            $ul->filter('.item-0')->each(function ($it) {
-                $it->filter('.profile')->each(function ($li) {
-                    global $names,$time,$message;
-                    foreach($names as $name){
-                        if($li->filter('.name_font_size')->text() == $name){
-                            $time = $li->filter('.shukkin_detail_time')->text();
-                            $message .= "\n" . $name . ":" . $time;
-                        }
-                    }
-                });
+            $ul->filter('#block_on_click')->each(function ($today) {
+                global $week;
+                $week = trim($today->filter('.wday')->text());
             });
         });
-        $uri = 'https://notify-api.line.me/api/notify';
-        $client = new Client();
-        $client->post($uri, [
-            'headers' => [
-                'Content-Type'  => 'application/x-www-form-urlencoded',
-                'Authorization' => 'Bearer eTKxIWnkJPriXwGcH0cNWYtAkvWQQ8NzrGM7x22DGBK',
-            ],
-            'form_params' => [
-                'message' => $message,
-            ]
-        ]);
+
+        if($week != file_get_contents("week.txt")){
+            file_put_contents("week.txt", trim($week));
+            $goutte->filter('#contents_main')->each(function ($ul) {
+                $ul->filter('.item-0')->each(function ($it) {
+                    $it->filter('.profile')->each(function ($li) {
+                        global $names,$time,$message;
+                        foreach($names as $name){
+                            if($li->filter('.name_font_size')->text() == $name){
+                                $time = $li->filter('.shukkin_detail_time')->text();
+                                $message .= "\n" . $name . ":" . $time;
+                            }
+                        }
+                    });
+                });
+            });
+            $uri = 'https://notify-api.line.me/api/notify';
+            $client = new Client();
+            $client->post($uri, [
+                'headers' => [
+                    'Content-Type'  => 'application/x-www-form-urlencoded',
+                    'Authorization' => 'Bearer eTKxIWnkJPriXwGcH0cNWYtAkvWQQ8NzrGM7x22DGBK',
+                ],
+                'form_params' => [
+                    'message' => $message,
+                ]
+            ]);
+        }
     }
 }
