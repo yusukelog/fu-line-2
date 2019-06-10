f35e045248f4493fd6621d78757b82fd8bc5f161
L <koga@3kaku.co.jp>
2019-06-10 15:12:20 +0900

    時間テーブルを使ってリレーション設定-2

--- app/Person.php
+++ app/Person.php
@@ -9,7 +9,7 @@ class Person extends Model
 {
     protected $guarded = array('id');
     function getTime(){
-        $time = optional($this->hasOne('App\Time')->first())->time;
+        $time = optional($this->hasOne('App\Time')->first())->time;//optional リレーションで呼び出すときに呼び出し先に値がない場合はNullで返す
         return unserialize($time);
     }
     public static $rules = array(
